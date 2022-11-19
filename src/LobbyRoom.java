6b42fd76b4f3d5cf6a47e5e6cf46ded11851d5ce
liuyang3141 <liu.yang3.141@gmail.com>
2022-11-19 12:45:17 -0800

    Implemented new functionality

--- src/LobbyRoom.java
+++ src/LobbyRoom.java
@@ -1,21 +1,92 @@
 import java.util.*;
 
 public class LobbyRoom {
-	private ArrayList<String> clientLobbyRoom;
+	private int numberOfRooms;
+	private ArrayList<ArrayList<String>> clientLobbyRoom;
 	
-	LobbyRoom(ArrayList<String> clientLobbyRoom) {
-		this.clientLobbyRoom = clientLobbyRoom;
+	LobbyRoom() {
+		this.clientLobbyRoom = new ArrayList<ArrayList<String>>();
+		numberOfRooms = 0;
 	}
 
-	public ArrayList<String> getClientLobbyRoom() {
+	public String toString() {
+		String output = "";
+		
+		output += numberOfRooms + "%";
+		
+		for (int x = 0; x < clientLobbyRoom.size(); ++x) {
+			output += x + "#" + clientLobbyRoom.get(x).size() + "#";
+			for (int y = 0; y < clientLobbyRoom.get(x).size(); ++y) {
+				output += clientLobbyRoom.get(x).get(y) + "#";
+			}
+		}
+		
+		return output;
+	}
+	
+	public ArrayList<ArrayList<String>> getClientLobbyRoom() {
 		return clientLobbyRoom;
 	}
 
-	public void setClientLobbyRoom(ArrayList<String> clientLobbyRoom) {
+	public void setClientLobbyRoom(ArrayList<ArrayList<String>> clientLobbyRoom) {
 		this.clientLobbyRoom = clientLobbyRoom;
 	}
 	
-	public void addPlayer(String player) {
-		clientLobbyRoom.add(player);
+	public void addRoom(String room) {
+		if (numberOfRooms > 0) {
+			clientLobbyRoom.add(numberOfRooms, new ArrayList<String>());
+		}
+		else {
+			clientLobbyRoom.add(0, new ArrayList<String>());
+		}
+		
+		++numberOfRooms;
+	}
+	
+	public boolean removeRoom() {
+		if (numberOfRooms > 0) {
+			clientLobbyRoom.remove(numberOfRooms - 1);
+			--numberOfRooms;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean addPlayer(int roomNumber, String player) {
+		if (clientLobbyRoom.get(roomNumber) != null) {
+			clientLobbyRoom.get(roomNumber).add(player);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean removePlayer(int roomNumber, String player) {
+		if (clientLobbyRoom.get(roomNumber) != null) {
+			
+			for (int x = 0; x < clientLobbyRoom.get(roomNumber).size(); ++x) {
+				if (clientLobbyRoom.get(roomNumber).get(x).equals("player")) {
+					clientLobbyRoom.get(roomNumber).remove(x);
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	public String playersInRoom(int roomNumber) {
+		String output = null;
+		
+		if (roomNumber <= this.numberOfRooms) {
+			output = "";
+			
+			for (int x = 0; x < clientLobbyRoom.get(roomNumber).size(); ++x) {
+				output += clientLobbyRoom.get(roomNumber).get(x) + ", ";
+			}
+		}
+		
+		return output;
 	}
 }
--- src/Message.java
+++ src/Message.java
@@ -1,106 +1,44 @@
 import java.io.Serializable;
 
 public class Message implements Serializable {
-	private static int count = 0;
-	private MessageType message;
-	private Room room;
-	private Player player;
-	private Login login;
-	private Register register;
-	private LobbyRoom lobbyRoom;
+	private String type;
+    private String status;
+    private String text;
+    private static final long serialVersionUID = 363945L;
 	
 	public Message() {
-		this.message = null;
-		this.room = null;
-		this.player = null;
-		this.login = null;
-		this.register = null;
-		this.lobbyRoom = null;
+		this.type = "undefined";
+		this.status = "undefined";
+		this.text = "undefined";
 	}
 	
-	public Message(MessageType message) {
-		this(message, null, null, null, null, null);
-	}
-	
-	public Message(MessageType message, Room room) {
-		this(message, room, null, null, null, null);
-	}
-	
-	public Message(MessageType message, Player player) {
-		this(message, null, player, null, null, null);
-	}
-	
-	public Message(MessageType message, Login login) {
-		this(message, null, null, login, null, null);
-	}
-	
-	public Message(MessageType message, Register register) {
-		this(message, null, null, null, register, null);
-	}
-	
-	public Message(MessageType message, LobbyRoom lobbyRoom) {
-		this(message, null, null, null, null, lobbyRoom);
-	}
-	
-	public Message(MessageType message, Room room, Player player, Login login, Register register, LobbyRoom lobbyRoom) {
-		this.message = message;
-		this.room = room;
-		this.player = player;
-		this.login = login;
-		this.register = register;
-		this.lobbyRoom = lobbyRoom;
-		++count;
+	public Message(String type, String status, String text) {
+		this.type = type;
+		this.status = status;
+		this.text = text;
 	}
 
-	public MessageType getMessage() {
-		return message;
+	public String getType() {
+		return type;
 	}
 
-	public void setMessage(MessageType message) {
-		this.message = message;
+	public void setType(String type) {
+		this.type = type;
 	}
 
-	public Room getRoom() {
-		return room;
+	public String getStatus() {
+		return status;
 	}
 
-	public void setRoom(Room room) {
-		this.room = room;
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
-	public Player getPlayer() {
-		return player;
+	public String getText() {
+		return text;
 	}
 
-	public void setPlayer(Player player) {
-		this.player = player;
-	}
-
-	public Login getLogin() {
-		return login;
-	}
-
-	public void setLogin(Login login) {
-		this.login = login;
-	}
-
-	public Register getRegister() {
-		return register;
-	}
-
-	public void setRegister(Register register) {
-		this.register = register;
-	}
-
-	public LobbyRoom getLobbyRoom() {
-		return lobbyRoom;
-	}
-
-	public void setLobbyRoom(LobbyRoom lobbyRoom) {
-		this.lobbyRoom = lobbyRoom;
-	}
-	
-	public int getCount() {
-		return count;
+	public void setText(String text) {
+		this.text = text;
 	}
 }
