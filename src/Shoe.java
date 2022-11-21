db37b77e1bafbefd5e47c618d1ea0ecb269552dc
liuyang3141 <liu.yang3.141@gmail.com>
2022-11-21 05:33:27 -0800

    Revised constructor to automatically generate cards when the shoe class is made.

--- src/Shoe.java
+++ src/Shoe.java
@@ -12,6 +12,7 @@ public class Shoe {
 		cardValue = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 		suits = new String[] {"Spade", "Heart", "Club", "Diamond"};
 		this.deck = new ArrayList<Card>();
+		generateCards();
 	}
 	
 	public Shoe(ArrayList<Card> deck) {
