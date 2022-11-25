bd70507005577222a6fe38819b6b2fd0d35c3d73
liuyang3141 <liu.yang3.141@gmail.com>
2022-11-24 21:18:56 -0800

    Uploading new set of files

---
+++ src/Start.java
@@ -0,0 +1,12 @@
+
+public class Start {
+	 /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) throws Exception {
+    	//String ip = "127.0.1.1";
+    	//int port = 59898;
+        Client client = new Client();
+        new ConnectGUI(client).setupConnectPanel();
+    }
+}
