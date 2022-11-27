b38f27505f4210d2974917ab9b096dcb53a85102
Eugene Lacatis <eugenejr14@gmail.com>
2022-11-26 17:29:53 -0800

    Ran once + Added TestRunner and imports to test cases

--- src/testing/MessageTesting.java
+++ src/testing/MessageTesting.java
@@ -1,5 +1,11 @@
 package testing;
 
+import main.Message;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
+
 public class MessageTesting {
 
 }
--- src/testing/ParserTesting.java
+++ src/testing/ParserTesting.java
@@ -1,5 +1,11 @@
 package testing;
 
+import main.Parser;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
+
 public class ParserTesting {
 
 }
