08be48ddb21086c04468d26f107cafc1e07ac7af
Eugene Lacatis <eugenejr14@gmail.com>
2022-11-27 13:56:10 -0800

    Implemented test cases for CardTesting

--- src/testing/CardTesting.java
+++ src/testing/CardTesting.java
@@ -8,4 +8,36 @@ import org.junit.jupiter.api.RepeatedTest;
 
 public class CardTesting {
 
+	@Test
+	public void ConstructorTest() {
+		int value = 5;
+		String suit = "diamond";
+		Card card = new Card(value, suit);
+		
+		assertTrue(card.getValue() == value && card.getSuit().equals(suit));
+	}
+	
+	@Test
+	public void GetterAndSetterTest() {
+		int value = 10;
+		String suit = "spade";
+		Card card = new Card(5, "heart");
+		
+		card.setSuit(suit);
+		card.setValue(value);
+		
+		assertTrue(card.getSuit().equals(suit) && card.getValue() == value);
+	}
+	
+	@Test
+	public void ToStringTest() {
+		int value = 3;
+		String suit = "club";
+		Card card = new Card(value, suit);
+		
+		String string1 = card.toString();
+		String string2 = Integer.toString(value) + "#" + suit + "#";
+		
+		assertTrue(string1.equals(string2));
+	}
 }
