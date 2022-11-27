5f2c2c3c2db48c025f239b96c36b500d93204f60
Eugene Lacatis <eugenejr14@gmail.com>
2022-11-26 17:32:16 -0800

    Added AllTests.java + TestRunner.java

---
+++ src/testing/AllTests.java
@@ -0,0 +1,13 @@
+package testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({
+	CardTesting.class, MessageTesting.class, ParserTesting.class, PlayerTesting.class, RoomTesting.class, ShoeTesting.class
+})
+public class AllTests {
+}
