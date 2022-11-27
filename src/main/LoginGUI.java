ab26dc48fb1e3e0bc91a406a8b74299fdac6be5c
bmurgic <brandon@murgic.com>
2022-11-26 21:37:06 -0800

    Merged Yang and brandon branch

--- src/main/LoginGUI.java
+++ src/main/LoginGUI.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
@@ -12,18 +13,18 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class LoginGUI {
-	
+
 	//private JFrame loginFrame;
 	private JFrame loginFrame = new JFrame("LoginGUI");
 	private JTextField txtUsername;
 	private JPasswordField txtPassword;
 	private JFrame frameLoginSystem;
 	private Client client;
-	
+
 	public JFrame getLoginFrame() {
 		return loginFrame;
 	}
-	
+
 	public static void main (String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -40,20 +41,20 @@ public class LoginGUI {
 			}
 		});
 	}
-	
+
 	public LoginGUI() {
 		startUp();
 	}
-	
+
 	public LoginGUI(Client client) {
 		startUp();
 		this.client = client;
 	}
-	
+
 	public void giveFalse(Boolean time) {
 		time = false;
 	}
-	
+
 	public void startUp() {
 		loginFrame = new JFrame();
 		loginFrame.setBounds(200, 200, 500, 300);
@@ -64,82 +65,88 @@ public class LoginGUI {
 		loginFrame.setTitle("BlackJack Login Window");
 		loginFrame.setResizable(false);
 		loginFrame.getContentPane().setBackground(Color.lightGray);
-		
+
 		JLabel signInLabel = new JLabel ("Sign In");
 		signInLabel.setBounds(375, 110, 150, 140);
 		loginFrame.getContentPane().add(signInLabel);
 		signInLabel.setFont(new Font("Dialogue", Font.BOLD, 40));
-		
+
 		JLabel usernameLabel = new JLabel ("Enter Username:");
 		usernameLabel.setBounds(250, 253, 100, 20);
 		loginFrame.getContentPane().add(usernameLabel);
-		
+
 		JLabel passwordLabel = new JLabel ("Enter Password:");
 		passwordLabel.setBounds(250, 303, 100, 20);
 		loginFrame.getContentPane().add(passwordLabel);
-		
+
 		txtUsername = new JTextField(20);
-		txtUsername.setBounds(350, 250, 200, 30); 
+		txtUsername.setBounds(350, 250, 200, 30);
 		loginFrame.getContentPane().add(txtUsername);
 		//txtUsername.setColumns(10);
-				
+
 		txtPassword = new JPasswordField();
 		txtPassword.setBounds(350, 300, 200, 30);
 		loginFrame.getContentPane().add(txtPassword);
-		
+
 		JButton loginButton = new JButton("Login");
 		loginButton.addActionListener (new ActionListener() {
-		
-		public void actionPerformed(ActionEvent e) {
-		
-		String password = txtPassword.getText();
-		String username = txtUsername.getText();
-		
-		
-		login();
-		}
-		
+
+			public void actionPerformed(ActionEvent e) {
+
+				String password = txtPassword.getText();
+				String username = txtUsername.getText();
+
+
+				login();
+			}
+
 		});
 		loginButton.setBounds(275, 375, 150, 45);
 		loginFrame.getContentPane().add(loginButton);
-		
+
 		JButton exitButton = new JButton("Exit");
 		exitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				frameLoginSystem = new JFrame("Exit");
 				// if (JOptionPane.showConfirmDialog(frameLoginSystem,  "Return to Homepage?", "Exit", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
 				if (JOptionPane.showConfirmDialog(frameLoginSystem,  "Confirm Exit?", "Exit", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
-					System.exit(0);
+					loginFrame.setVisible(false);
+					loginFrame.dispose();
+					System.out.println("Client ip = " + client.socket);
+
+					RegisterOrLoginGUI window = new RegisterOrLoginGUI(client);
+					window.getRegisterFrame().setLocationRelativeTo(null); // center on screen
+					window.getRegisterFrame().setVisible(true); // make visible
 				}
 			}
 		});
 		exitButton.setBounds(475, 375, 150, 45);
 		loginFrame.getContentPane().add(exitButton);
 	}
-	
+
 	public void login() {
 		// Get user input from LoginGUI
 		String password = txtPassword.getText();
 		String username = txtUsername.getText();
-		
+
 		// Execute login by sending a login message to the server
 		client.login(username, password);
-		
+
 		// Busy wait until server gets the message, responds, then server adjusts the value of client.login to either 1 or -1
 		while(client.getLogin() == 0) {
 			System.out.println("client login = " + client.getLogin());
-		}	
-		
+		}
+
 		// Login success, create LobbyGUI and close LoginGUI
 		if (client.getLogin() == 1) {
 			System.out.println("client login = " + client.getLogin());
-			
-			LobbyGUI lobbyGUI = new LobbyGUI(client); 
+
+			LobbyGUI lobbyGUI = new LobbyGUI(client);
 			client.setLobbyGUI(lobbyGUI);
 			lobbyGUI.setupLobbyPanel();
 			loginFrame.dispose();
-			
+
 		}
 		// Login failed. Reset client.login = 0 so the busy wait loop will work again.
 		else if (client.getLogin() == -1){
