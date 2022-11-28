c53c85baa8a8c877afa1a0bb666653f456aea748
Eugene Lacatis <eugenejr14@gmail.com>
2022-11-28 14:50:22 -0800

    Added constructors, dealCard(), and toString() tests

--- src/testing/ShoeTesting.java
+++ src/testing/ShoeTesting.java
@@ -3,18 +3,38 @@ package testing;
 import main.Shoe;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.Test;
 
 
 public class ShoeTesting {
-	
 	@Test
 	public void GetterAndSetterTest() {
 		
 		Shoe shoe = new Shoe();
 		
-		assertTrue(shoe.getShoeSize() == 312);
-		assertTrue(shoe.getCutPoint() == 70);
-		assertTrue(shoe.getNumOfDecks() == 6);
+		assertEquals(shoe.getShoeSize(), 312);
+		assertEquals(shoe.getCutPoint(), 70);
+		assertEquals(shoe.getNumOfDecks(), 6);
+		shoe.getDeck();
+	}
+	@Test
+	public void dealCardTest() {
+		
+		Shoe shoe1 = new Shoe();
+		Shoe shoe2 = new Shoe(shoe1.getDeck());
+		shoe2.dealCard();
+		
+		shoe2.clearDeck();
+		shoe2.dealCard();
+	}
+	@Test
+	public void toStringTest() {
+		String output = "";
+		Shoe shoe1 = new Shoe();
+		for (int i = 0; i < shoe1.getDeck().size(); ++i) {
+			output += shoe1.getDeck().get(i).toString();
+		}
+		assertTrue(output.equals(shoe1.toString()));
 	}
 }
