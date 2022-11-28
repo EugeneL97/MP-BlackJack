175528ba25f225ed70e28af447963e968d1227f4
Eugene Lacatis <eugenejr14@gmail.com>
2022-11-27 17:47:51 -0800

    Removed unnecessary imports

--- src/testing/CardTesting.java
+++ src/testing/CardTesting.java
@@ -4,7 +4,6 @@ import main.Card;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
 
 public class CardTesting {
 
--- src/testing/MessageTesting.java
+++ src/testing/MessageTesting.java
@@ -4,7 +4,7 @@ import main.Message;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
+
 
 public class MessageTesting {
 	@Test
