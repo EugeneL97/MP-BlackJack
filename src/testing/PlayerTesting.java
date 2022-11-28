26eb65f59b10ae72b9bb2daebf7f1e8968a411a5
Eugene Lacatis <eugenejr14@gmail.com>
2022-11-27 18:42:44 -0800

    Implemented test cases for PlayerTesting

--- src/testing/PlayerTesting.java
+++ src/testing/PlayerTesting.java
@@ -7,5 +7,43 @@ import org.junit.Test;
 import org.junit.jupiter.api.RepeatedTest;
 
 public class PlayerTesting {
-
+	@Test
+	public void ConstructorTest() {
+		String username = "Jimmy";
+		int accountBalance = 5000;
+		Player player1 = new Player(username, accountBalance);
+		assertTrue(player1.getUsername().equals(username));
+		assertTrue(player1.getAccountBalance() == accountBalance);
+	}
+	
+	@Test
+	public void GetterAndSetterTest() {
+		String username = "Bobby";
+		int roomNumber = 1;
+		int playerState = 1;
+		int wager = 100;
+		int seatIndex = 1;
+		int accountBalance = 5000;
+		int currentAction = 0;
+		String score = "1000";
+		
+		Player player1 = new Player("Jimmy Jangles", 10000);
+		player1.setUsername(username);
+		player1.setRoomNumber(roomNumber);
+		player1.setPlayerState(playerState);
+		player1.setWager(wager);
+		player1.setSeatIndex(seatIndex);
+		player1.setAccountBalance(accountBalance);
+		player1.setCurrentAction(currentAction);
+		player1.setScore(score);
+		
+		assertTrue(player1.getUsername().equals(username));
+		assertTrue(player1.getRoomNumber() == roomNumber);
+		assertTrue(player1.getPlayerState() == playerState);
+		assertTrue(player1.getWager() == wager);
+		assertTrue(player1.getSeatIndex() == seatIndex);
+		assertTrue(player1.getAccountBalance() == accountBalance);
+		assertTrue(player1.getCurrentAction() == currentAction);
+		assertTrue(player1.getScore().equals(score));
+	}
 }
